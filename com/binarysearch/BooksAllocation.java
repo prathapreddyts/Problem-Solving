7d41267d2a9140e18561b1fbd57778d5529f943d
prathapreddyts <tsprathapreddy09@gmail.com>
2025-01-31 12:45:21 +0530

    added booksAllocation and Median of two sorted arrays.

---
+++ com/binarysearch/BooksAllocation.java
@@ -0,0 +1,46 @@
+package com.binarysearch;
+
+import java.rmi.dgc.VMID;
+
+public class BooksAllocation {
+    public static int findPages(int[] books, int students) {
+        if (books.length < students) {
+            return -1;
+        }
+        int start = 0;
+        int end = 0;
+        for (int book : books) {
+            start = Math.max(start, book);
+            end += book;
+        }
+        int res = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (bookAllocation(books, mid, students)) {
+                res = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public static boolean bookAllocation(int[] books, int maxPages, int students) {
+        int pages = 0;
+        int studentCount = 1;
+       for(int i=0;i<books.length;i++){
+           if(pages+books[i]<= maxPages){
+               pages+=books[i];
+           }else{
+               studentCount++;
+               if(studentCount>students){
+                   return false;
+               }
+               pages=books[i];
+           }
+       }
+        return true;
+    }
+
+}
---
+++ com/binarysearch/MedianOfSortedArrays.java
@@ -0,0 +1,40 @@
+package com.binarysearch;
+
+public class MedianOfSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums2.length < nums1.length) {
+            return findMedian(nums2, nums1);
+        }
+        return findMedian(nums1, nums2);
+    }
+
+    public double findMedian(int[] arr1, int[] arr2) {
+        int low = 0;
+        int high = arr1.length;
+        int totalLength = arr1.length + arr2.length;
+
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = (totalLength + 1) / 2 - mid1;
+
+            int l1 = (mid1 == 0) ? Integer.MIN_VALUE : arr1[mid1 - 1];
+            int r1 = (mid1 == arr1.length) ? Integer.MAX_VALUE : arr1[mid1];
+
+            int l2 = (mid2 == 0) ? Integer.MIN_VALUE : arr2[mid2 - 1];
+            int r2 = (mid2 == arr2.length) ? Integer.MAX_VALUE : arr2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (totalLength % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0; // Ensure double division
+                } else {
+                    return Math.max(l1, l2);
+                }
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
+        }
+        return -1;
+    }
+}
